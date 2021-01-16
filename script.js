563e34a2d443b6b71bf35914fd209cfa3cecb356
Nagaraju <nagarajrepala@gmail.com>
2021-01-16 11:22:30 +0530

    Timer

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var x = setInterval(function () {
 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("timer").innerHTML = "CLOSED";
   }
 }, 1000);
 
